1e959470ee5f2f3f9dd78d71df35f5664a7450bc
Erik <erik.benton@hotmail.com>
2020-07-09 10:21:11 -0700

    quick solution for first problem 'coprime?'

--- Software Engineering Foundations/Advanced Ruby/Exercises/01_coprime.rb
+++ Software Engineering Foundations/Advanced Ruby/Exercises/01_coprime.rb
@@ -3,6 +3,17 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+def coprime?(num_1, num_2)
+  if num_1 < num_2
+    min = num_1
+  else
+    min = num_2
+  end
+  (2..min).each do |div|
+    return false if num_1 % div == 0 && num_2 % div == 0
+  end
+  true
+end
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
