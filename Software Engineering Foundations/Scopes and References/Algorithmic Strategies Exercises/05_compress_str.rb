5846ead3a256813e0577b32d6f84a86324eb974e
Erik <erik.benton@hotmail.com>
2020-07-09 14:36:23 -0700

    finished 05_compress_str exercise but looks a little sloppy...

--- Software Engineering Foundations/Scopes and References/Algorithmic Strategies Exercises/05_compress_str.rb
+++ Software Engineering Foundations/Scopes and References/Algorithmic Strategies Exercises/05_compress_str.rb
@@ -2,6 +2,27 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 
+def compress_str(str)
+  res = [str[0]]
+  count = 1
+  (1...str.length).each do |idx|
+    char = str[idx]
+    if char.eql?(res.last)
+      count += 1
+    else
+      res.push(count, char)
+      count = 1
+    end
+  end
+  res.push(count)
+  idx = 0
+  (res.length/2).times do
+    res[idx], res[idx + 1] = res[idx + 1], res[idx]
+    idx += 2
+  end
+  res.filter! { |el| el != 1 }
+  res.join("")
+end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
